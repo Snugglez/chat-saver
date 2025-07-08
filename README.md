c65582b20dc85faa58bf139700a4b2d06b5fed98
Snugglez <32309975+Snugglez@users.noreply.github.com>
2025-07-08 13:30:04 -0400

    untested edit
    should work, removes redundant variable that was wrong anyways and removed the need for the command since it acted as more of a bool anyways that couldnt be toggled off

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # chat-saver
 
-Saves your chat settings with `/8 chat`
-
-Once you have saved chat settings, the mod will take over and remove any interactions between server and client keeping your chat settings entirely local
+Takes over and removes any interaction between server and client keeping your chat settings entirely local
--- index.js
+++ index.js
@@ -2,8 +2,6 @@ module.exports = function reee(d) {
     d.dispatch.addDefinition('C_SAVE_CLIENT_CHAT_OPTION_SETTING', 0, __dirname + '//res//C_SAVE_CLIENT_CHAT_OPTION_SETTING.def', true)
     d.dispatch.addDefinition('S_REPLY_CLIENT_CHAT_OPTION_SETTING', 0, __dirname + '//res//S_REPLY_CLIENT_CHAT_OPTION_SETTING.def', true)
 
-    let chatOptions = []
-
     d.hook('C_REQUEST_CLIENT_CHAT_OPTION_SETTING', 'event', () => {
         if (!d.settings.chatOptions.tabs) return
         d.send('S_REPLY_CLIENT_CHAT_OPTION_SETTING', 0, d.settings.chatOptions)
@@ -11,20 +9,12 @@ module.exports = function reee(d) {
     })
 
     d.hook('C_SAVE_CLIENT_CHAT_OPTION_SETTING', 0, (e) => {
-        chatOptions = { tabs: e.tabs, channels: e.channels }
-        if (d.settings.chatOptions.tabs) {
-            d.settings.chatOptions = { tabs: e.tabs, channels: e.channels }
-            return false
-        }
+        d.settings.chatOptions = e
+        return false
     })
 
     d.hook('S_REPLY_CLIENT_CHAT_OPTION_SETTING', 'event', () => {
         if (d.settings.chatOptions.tabs) return false
-    })
-
-    d.command.add('chat', () => {
-        d.settings.chatOptions = chatOptions
-        d.command.message('chat saved')
     })
 
     //I raw logged these for a week and never saw this shit trigger, so im just gonna block it to be safe 
